8a347cb09e17ddd05537c08b638825fc57351c56
georgeDuSH <georgedush@163.com>
2023-01-06 17:28:01 +0800

    svd, biased svd w/wo random negative sampling

--- model/svd.py
+++ model/svd.py
@@ -32,9 +32,6 @@ class FunkSVD(nn.Module):
             return -pos_rat
 
         neg_emb = self.embedding_item(neg_item)
-        # if len(neg_emb.shape) == 2:
-        #     r, c = neg_emb.shape
-        #     neg_emb = neg_emb.view(r, 1, self.k_dim)
 
         user_emb = self.embedding_user(user).view(-1, self.k_dim, 1) # transform shape
         neg_rat = -(torch.bmm(neg_emb, user_emb)).view(-1) # use bmm to calcualte score
@@ -45,20 +42,20 @@ class FunkSVD(nn.Module):
 class BiasedFunkSVD(nn.Module):
     """ Matrix Factorization based method, referring to FunkSVD
     """
-    def __init__(self, n_user, n_item, k_dim, user_bias, item_bias, negative_sampling:bool):
+    def __init__(self, n_user, n_item, k_dim, user_bias, item_bias, global_bias, negative_sampling:bool):
         super(BiasedFunkSVD, self).__init__()
-        self.k_dim=k_dim
+        self.k_dim = k_dim
         self.negative_sampling = negative_sampling # if training with negative sampling
         # bias
-        self.bias_user = nn.Parameter(tensor(list(user_bias.values())))
-        self.bias_item = nn.Parameter(tensor(list(item_bias.values())))
-        self.bias = nn.Parameter(tensor([0.]), requires_grad=True)
+        self.bias_user = nn.Parameter(tensor(list(user_bias.values())), requires_grad=False)
+        self.bias_item = nn.Parameter(tensor(list(item_bias.values())), requires_grad=False)
+        self.bias = nn.Parameter(tensor([global_bias]), requires_grad=False)
         # embeddings of interest
         self.embedding_user = nn.Embedding(n_user, k_dim)
         self.embedding_item = nn.Embedding(n_item, k_dim)
         # init param
-        self.embedding_user.weight.data.uniform_(0, 1)
-        self.embedding_item.weight.data.uniform_(0, 1)
+        self.embedding_user.weight.data.uniform_(0, 2)
+        self.embedding_item.weight.data.uniform_(0, 2)
 
     def pred_score(self, user, item):
         user = tensor([user])
@@ -82,15 +79,11 @@ class BiasedFunkSVD(nn.Module):
             return -pos_rat
 
         neg_emb = self.embedding_item(neg_item)
-        if len(neg_emb.shape) == 2:
-            r, c = neg_emb.shape
-            neg_emb = neg_emb.view(r, 1, self.k_dim)
-
         user_emb = self.embedding_user(user).view(-1, self.k_dim, 1) # transform shape
         neg_size = neg_emb.shape[1]
 
         neg_rat = -(
-                torch.bmm(neg_emb, user_emb)[:,:,0]
+                torch.bmm(neg_emb, user_emb)[:, :, 0]
                 + self.bias_user[user].view(-1, 1).expand(-1, neg_size)
                 + self.bias_item[neg_item]
         ).view(-1) # use bmm to calcualte score
--- utils/preprocessor.py
+++ utils/preprocessor.py
@@ -1,3 +1,8 @@
+
+flatten_score = lambda rat_dic: [item for user in rat_dic
+                                 for item in rat_dic[user].values()]
+
+
 def compute_rat_bias(rat_dict, user_size, item_size):
     user_bias = dict(zip(range(user_size)
                          , [0]*user_size))
@@ -6,17 +11,20 @@ def compute_rat_bias(rat_dict, user_size, item_size):
     item_rat_dict = dict(zip(range(item_size)
                              , [[] for _ in range(item_size)]))
 
+    all_scores = flatten_score(rat_dict)
+    average_score = sum(all_scores)/len(all_scores)
+
     for user in rat_dict:
         N = len(rat_dict[user])
         for item in rat_dict[user]:
             # add item rating into
             item_rat_dict[item].append(rat_dict[user][item])
         # compute user's rating
-        user_bias[user] = sum(rat_dict[user].values())/N
+        user_bias[user] = sum(rat_dict[user].values())/N - average_score
 
     for item in item_rat_dict: # compute item bias
         N = len(item_rat_dict[item])
         if N!=0:
-            item_bias[item] = sum(item_rat_dict[item])/N
+            item_bias[item] = sum(item_rat_dict[item])/N - average_score
 
-    return user_bias, item_bias
+    return average_score, user_bias, item_bias
