70cf32924f9198f615e07b43709efbbe894f438d
georgeDuSH <georgedush@163.com>
2023-01-09 21:22:45 +0800

    Compare SVD, PMF, and Logistic PMF

--- recom/model/MF.py
+++ recom/model/MF.py
@@ -2,7 +2,7 @@ import sys, os
 sys.path.append(os.getcwd())
 
 # pack load
-from torch import LongTensor, Tensor
+from torch import LongTensor, Tensor, row_stack
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -218,6 +218,40 @@ class PMF(_MF):
         # init param
         nn.init.normal_(self.embedding_user.weight, mean=0, std=std_user)
         nn.init.normal_(self.embedding_item.weight, mean=0, std=std_item)
+
+    def pred(self, user, item):
+        from torch import LongTensor
+
+        # Tensorize
+        if isinstance(user, int): user = LongTensor([user])
+        if isinstance(user, list): user = LongTensor(user)
+        if isinstance(item, int): item = LongTensor([item])
+        if isinstance(item, list): item = LongTensor(item)
+
+        user_emb = self.embedding_user(user)
+        item_emb = self.embedding_item(item)
+
+        return user_emb @ item_emb.T # dot product
+
+    def forward(self, user, item):
+        return (self.embedding_user(user) \
+                * self.embedding_item(item)).sum(1)
+
+class LogisticPMF(_MF):
+    """ Probabilistic Matrix Factorization
+        - [ref]: Mnih, A., & Salakhutdinov, R. R. (2007). Probabilistic matrix factorization. Advances in neural information processing systems, 20.
+        - [url]: https://proceedings.neurips.cc/paper/2007/file/d7322ed717dedf1eb4e6e52a37ea7bcd-Paper.pdf
+    """
+    def __init__(self, n_user, n_item, k_dim
+                 , std_user=1, std_item=1):
+        super(_MF, self).__init__()
+        self.items = list(range(n_item))
+        # embeddings of interest
+        self.embedding_user = nn.Embedding(n_user, k_dim)
+        self.embedding_item = nn.Embedding(n_item, k_dim)
+        # init param
+        nn.init.normal_(self.embedding_user.weight, mean=0, std=std_user)
+        nn.init.normal_(self.embedding_item.weight, mean=0, std=std_item)
         self.sigmoid = F.sigmoid
 
     def pred(self, user, item):
@@ -287,3 +321,122 @@ class PMF(_MF):
         self.last_train_loss = losses
 
         return self
+
+
+class ConstrainedPMF(_MF):
+    def __init__(self, n_user, n_item, k_dim
+                 , std_user=1, std_item=1):
+        super(_MF, self).__init__()
+        self.n_user = n_user # placeholder
+        # embeddings of interest
+        self.user_offset = nn.Embedding(n_user, 1)
+        self.latent_similarity = nn.Embedding(n_item, k_dim)
+        self.embedding_item = nn.Embedding(n_item, k_dim)
+        # init param
+        nn.init.normal_(self.user_offset.weight, mean=0, std=0)
+        nn.init.normal_(self.latent_similarity.weight, mean=0, std=std_user)
+        nn.init.normal_(self.embedding_item.weight, mean=0, std=std_item)
+        # rating history
+        self.rat_hist = None
+        # layer
+        self.sigmoid = F.sigmoid
+
+    def pred(self, user, item):
+        from torch import LongTensor
+
+        # Tensorize
+        if isinstance(user, int): user = [user]
+        if isinstance(item, int): item = LongTensor([item])
+        if isinstance(item, list): item = LongTensor(item)
+
+        user_offset = self.user_offset(LongTensor(user))
+
+        user_emb = row_stack([
+            self.latent_similarity(self.rat_hist[int(u)]).mean(0)
+            for u in user
+        ])
+
+        item_emb = self.embedding_item(item)
+
+        # dot product
+        return (user_emb + user_offset) @ item_emb.T
+
+    def forward(self, user_ix, user_emb, item):
+        user_offset = self.user_offset(user_ix)
+        embedding_user = user_offset + user_emb
+
+        # print(user_offset.is_cuda)
+        # print(embedding_user.is_cuda)
+        # print(self.embedding_item(item).is_cuda)
+
+        return (embedding_user * self.embedding_item(item)).sum(1)
+
+    def fit(self, train_dict, opt_fn
+            , batch_size=128
+            , n_epochs=128
+            , user_per_ep=128, item_per_ep=32
+            , report_interval=10, use_cuda=False):
+
+        import time
+        from torch import autograd, LongTensor, device
+
+        if use_cuda:
+            compute_device = device('cuda')
+            self.cuda()
+            self.latent_similarity.to('cpu')
+        else:
+            compute_device = device('cpu')
+
+        self.rat_hist = self.get_rating_history(train_dict)
+        proj_train_dict = rating_min_max_scalar(train_dict)
+
+        losses = []
+
+        t0 = time.time()
+        for epoch in range(n_epochs):
+            train_data = mf_data_loader(proj_train_dict, user_per_ep, item_per_ep, batch_size)
+
+            for i, batch in enumerate(train_data):
+                user, item, rate = batch
+
+                self.zero_grad()
+
+                # variablize
+                user_emb = row_stack([
+                    self.latent_similarity(self.rat_hist[int(u)]).mean(0) \
+                    for u in user
+                ]).to(compute_device)
+
+                user_ix = autograd.Variable(LongTensor(user)).to(compute_device)
+
+                item = autograd.Variable(LongTensor(item)).to(compute_device)
+                rate = autograd.Variable(rate).to(compute_device)
+
+                # use sigmoid as projection
+                preds = self(user_ix, user_emb, item)
+                loss = RMSE(rate, self.sigmoid(preds))
+
+                loss.backward()
+                opt_fn.step()
+                losses.append(loss.data.to(compute_device).tolist())
+
+            if report_interval > 0 \
+                    and ((epoch+1) % report_interval == 0):
+                t1=time.time()
+                print(f'Epoch: {epoch+1}, Time: {round(t1-t0,2)}, /Average loss {round(sum(losses[-report_interval:])/report_interval, 5)}')
+                t0=time.time()
+
+        self.last_train_loss = losses
+
+        return self
+
+    def get_rating_history(self, rating):
+        from torch import LongTensor
+
+        return dict(
+            (user, LongTensor(list(rating[user].keys())))
+            for user in rating
+        )
+
+    def set_history(self, rating)->None:
+        self.rat_hist = self.get_rating_history(rating)
